6d1859cebbe0663d38b1c99d6273bd20a89aa877
LSantanderGit <lucasmsantander136@gmail.com>
2023-12-06 21:28:10 -0300

    Feature: Material Table

--- src/app/components/app-lista-empleados/app-lista-empleados.component.ts
+++ src/app/components/app-lista-empleados/app-lista-empleados.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import empleadosData from '../../../assets/data/empleados/empleados.json';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 
 interface Empleado{
   id: number;
@@ -10,9 +11,11 @@ interface Empleado{
 }
 
 @Component({
-  selector: 'app-app-lista-empleados',
+  selector: 'app-app-lista-empleados',  
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, 
+    MatTableModule],
+
   templateUrl: './app-lista-empleados.component.html',
   styleUrl: './app-lista-empleados.component.scss'
 })
